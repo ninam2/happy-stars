492ad6bdc3bad19b6417cb517907d1dead141496
nmuelle2 <nina-marie.mueller@axelspringer.de>
2015-04-13 14:27:58 +0200

    "hallo" -> starsresource.get()

--- src/main/resources/public/application.js
+++ src/main/resources/public/application.js
@@ -6,7 +6,39 @@ angular.module('happy', ['ngResource', 'ngSanitize'])
         var starsResource = $resource('stars');
         var starResource = $resource('stars/:id');
 
- 
+        // STAR ALIVE CHECK
+        $scope.alive = false;
+
+        $scope.checkAlive = function () {
+            var promise = versionResource.get().$promise;
+            promise.then(function (infos) {
+                console.info('It is alive!');
+                $scope.alive = true;
+            });
+            promise.catch(function (error) {
+                console.error('It is dead :(');
+                console.error(error);
+                $scope.alive = false;
+            });
+        };
+
+        // execute checkALive every 30 seconds
+        $interval($scope.checkAlive, 30 * 1000);
+        // END ALIVE CHECK
+
+        // START: Stars!
+        $scope.stars = {};
+        $scope.getStars = function () {
+            var promise = starsResource.get().$promise;
+            promise.then(function (result) {
+                console.info(result);
+                $scope.stars = result;
+            });
+            promise.catch(function () {
+                console.error(error);
+                $scope.stars = {};
+            });
+        };
 
 
 
